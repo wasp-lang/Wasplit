40e46862f2017bb44ae967755b995d7c7fc76579
Matija Sosic <matija.sosic@gmail.com>
2022-10-07 13:27:51 +0200

    Added auth buttons to the landing page.

--- ext/LandingPage.js
+++ ext/LandingPage.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const LandingPage = () => {
-    return "Landing!"
+    return (
+        <div>
+            <nav>
+                <Link to='login' className='mx-2 my-1 p-2 bg-blue-600 text-white'>Log in</Link>
+                <Link to='signup' className='mx-2 my-1 p-2 bg-blue-600 text-white'>Sign up</Link>
+            </nav>
+            Welcome to Wasplit!
+        </div>
+    )
 }
 
 export default LandingPage
